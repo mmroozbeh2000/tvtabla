5b2d93db8386ab8754f0d56410628f7f940e0aca
Gustav Gisselfeldt <gustav.gisselfeldt@gmail.com>
2021-04-08 16:25:16 +0200

    test

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,14 +7,7 @@ const Sidebar = () => {
     return (
 
         <div className="menu">
-<<<<<<< HEAD
-        <a class="menu__link" href="">SVT 1</a>
-             <a class="menu__link" href="">SVT 2</a>
-             <a class="menu__link" href="">SVT Barn</a>
-             <a class="menu__link" href="">SVT Kuskapskanalen</a>
-             <a class="menu__link" href="">SVT 24</a>
-=======
-
+        
             <a href="url" class="menu__link">svt 1</a>
             <a href="url" class="menu__link">svt 2</a>
             <a href="url" class="menu__link">kanal 3</a>
@@ -22,7 +15,6 @@ const Sidebar = () => {
             <a href="url" class="menu__link">tv 6</a>
             <a href="url" class="menu__link">eurosport</a>
 
->>>>>>> master
         </div>
 
     )
