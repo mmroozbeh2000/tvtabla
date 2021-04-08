c1cf770128691ef91c94cc10c4e705a01db059c0
Gustav Gisselfeldt <gustav.gisselfeldt@gmail.com>
2021-04-08 16:20:22 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import ProgramList from './components/ProgramList';
 import Sidebar from './components/Sidebar';
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div>
-      <Sidebar />
-      <ProgramList />
-    
-    </div>
-=======
 import Button from './components/Menubutton';
 
 function App() {
   return (
     <div><Button /><Sidebar /><ProgramList /></div>
->>>>>>> master
   );
 }
 
